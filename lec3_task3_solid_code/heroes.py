a600ba6135f9e3197a50f4297df57bac80449bb3
Sxarmat <sxarmat@yandex.ru>
2022-07-14 17:06:13 +0500

    lec3_task2.py and heroes.py changed

--- lec3_task3_solid_code/heroes.py
+++ lec3_task3_solid_code/heroes.py
@@ -3,7 +3,7 @@ from skills import Laser, Gun, Fighter
 from mass_media import MassMedia
 
 
-class SuperHero(Gun):
+class SuperHero:
 
     def __init__(self, name, can_use_ultimate_attack=True):
         self.name = name
@@ -15,23 +15,28 @@ class SuperHero(Gun):
         self.finder.get_antagonist(place)
 
     def attack(self):
-        self.fire_a_gun()
+        arsenal = Gun()
+        arsenal.fire_a_gun()
 
     def ultimate(self):
-        self.fire_a_machine_gun()
+        arsenal = Gun()
+        arsenal.fire_a_machine_gun()
 
     def create_news(self, place):
         self.news_creator.create_news(self.name, place)
 
 
-class Superman(Laser, Fighter, SuperHero):
+class Superman(SuperHero):
 
-    def __init__(self, can_use_ultimate_attack=True):
-        super(Superman, self).__init__('Clark Kent', can_use_ultimate_attack)
+    def __init__(self):
+        super(Superman, self).__init__('Clark Kent', True)
 
     def attack(self):
-        return self.kick()
+        arsenal = Fighter()
+        arsenal.kick()
 
     def ultimate(self):
-        return self.incinerate_with_lasers()
+        arsenal = Laser()
+        arsenal.incinerate_with_lasers()
+
 
